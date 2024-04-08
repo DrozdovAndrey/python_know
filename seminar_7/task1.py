e767330fc869cd49342061241a200158a67af34c
DrozdovAndrey <drozdov_andrey@list.ru>
2024-04-08 14:59:25 +0300

    added task1

---
+++ seminar_7/task1.py
@@ -0,0 +1,16 @@
+# Задание №1
+# ✔ Напишите функцию, которая заполняет файл
+# (добавляет в конец) случайными парами чисел.
+# ✔ Первое число int, второе - float разделены вертикальной чертой.
+# ✔ Минимальное число - -1000, максимальное - +1000.
+# ✔ Количество строк и имя файла передаются как аргументы функции.
+from random import randint as rint, uniform as rfloat
+
+
+def my_func(cout_lines, name_file):
+    with open(name_file, 'w', encoding='utf-8') as f:
+        for i in range(cout_lines):
+            f.writelines(f'{i+1}. int: {rint(-1000, 1000)} float: {rfloat(-1000, 1000)}\n')
+
+
+my_func(5, 'file_1.txt')
