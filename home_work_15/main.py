ec12e16350b607d639f1bc81021c7f0f349e52e0
DrozdovAndrey <drozdov_andrey@list.ru>
2024-05-28 12:09:43 +0300

    added info

--- home_work_15/main.py
+++ home_work_15/main.py
@@ -6,9 +6,12 @@ if __name__ == '__main__':
     parser.add_argument('param', metavar='width height', type=int, nargs=2,
                         help='enter width height separated by a space')
 
-    args = parser.parse_args()
-    r6 = Rectangle(*args.param)
-    print(r6)
+    # Для запуска из консоли раскоментировать следующие 3 строки и прописать команду к в командной строке:
+    # python home_work_15/main.py 10 12
+
+    # args = parser.parse_args()
+    # r6 = Rectangle(*args.param)
+    # print(r6)
 
     r2 = Rectangle(3, 5)
     r3 = Rectangle(15, 200)
@@ -22,6 +25,11 @@ if __name__ == '__main__':
     r5.width = 5
     r5.height = 3
     var_eq2 = r5 == r2
+
+    # Строки кода которые вызовут ошибку ниже этого комментария, для проверки вывода логирования всех ошибок,
+    # следует закоментировать по очереди каждую из последующих строк и запустить программу заново:
+
     r1 = Rectangle(1, -5)
+    r7 = Rectangle(-1, 5)
     r5.width = -5
     r5.height = -3
